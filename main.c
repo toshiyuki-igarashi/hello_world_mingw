4b6cfe725ef28c8d8516e71fa78f52c41341bd95
toshiyuki-igarashi <45117767+toshiyuki-igarashi@users.noreply.github.com>
2020-05-04 16:03:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,10 +7,17 @@
 Hello World!
 _POSIX_C_SOURCE:31069
 
-*/
+ * but it fails on github action
+compile main.c
+main.c: In function 'main':
+main.c:14:34: error: '_POSIX_C_SOURCE' undeclared (first use in this function)
+   printf("_POSIX_C_SOURCE:%x\n", _POSIX_C_SOURCE);
+                                  ^~~~~~~~~~~~~~~
+main.c:14:34: note: each undeclared identifier is reported only once for each function it appears in
+##[error]Process completed with exit code 1.*/
 
 int main(void){
   printf("Hello World!\n");
-  printf("_POSIX_C_SOURCE:%x\n", _POSIX_C_SOURCE);
+/*  printf("_POSIX_C_SOURCE:%x\n", _POSIX_C_SOURCE);*/
   return 0;
 }
