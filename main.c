f525f8492864a44e295e8b4e6451ad60a751b19e
Toshiyuki Igarashi <ged00475@nifty.com>
2020-05-04 15:56:40 +0900

    check gcc version

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/*
+ * expected output :
+ *
+
+Hello World!
+_POSIX_C_SOURCE:31069
+
+*/
+
 int main(void){
-  printf("Hello World!¥n");
+  printf("Hello World!\n");
+  printf("_POSIX_C_SOURCE:%x\n", _POSIX_C_SOURCE);
   return 0;
 }
